8fc3ec343266c6411f7670199baff13923bb0e7c
yasmimcmpos <yasmimfer987@outlook.com>
2025-06-03 00:57:24 -0300

    resolvendo

---
+++ escola.py
@@ -0,0 +1,17 @@
+#Dado os nomes dos alunos junto com a série em que estão, crie uma lista para a escola.
+
+#Adicione o nome de um aluno à lista de uma nota:
+#"Adicione Jim à 2ª série.OK."
+#Obtenha uma lista de todos os alunos matriculados em uma série:
+#"Quais alunos estão na 2ª série?"
+#"Nós só temos Jim agora."
+#Obtenha uma lista classificada de todos os alunos em todas as séries. As notas devem ser classificadas como 1, 2, 3, etc.,
+# e os alunos dentro de uma série devem ser classificados em ordem alfabética por nome.
+#"Quem está matriculado na escola agora?"
+#"Deixe-me pensar. Temos Anna, Barb e Charlie na 1ª série, Alex, Peter e Zoe na 2ª série e Jim na 5ª série.
+# Então a resposta é: Anna, Barb, Charlie, Alex, Peter, Zoe e Jim.
+#Observe que todos os nossos alunos têm apenas um nome (é uma cidade pequena, o que você quer?),
+# E cada aluno não pode ser adicionado mais de uma vez a uma série ou à lista. Se um teste tentar adicionar o mesmo aluno
+# mais de uma vez, sua implementação deverá indicar que isso está incorreto.
+#Os testes para este exercício esperam que sua lista escolar seja implementada por meio de uma Escola em Python.
+# Se você não estiver familiarizado com classes em Python, as classes da documentação do Python são um bom lugar para começar.class
--- ex1.py
+++ ex1.py
@@ -1,4 +1,4 @@
-#Se listarmos todos os números naturais abaixo de 10 que são múltiplos de 3 ou 5, obtemos 3, 5, 6 e 9. 
+#Se listarmos todos os números naturais abaixo de 10 que são múltiplos de 3 ou 5, obtemos 3, 5, 6 e 9.
 # A soma desses múltiplos é 23. Encontre a soma de todos os múltiplos de 3 ou 5 abaixo de 1000.
 
 
