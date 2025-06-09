7809abc34a3674214a7a71558a57ff5914a7b7ff
yasmimcmpos <yasmimfer987@outlook.com>
2025-06-09 17:06:22 -0300

    resolvendo

---
+++ ex3.py
@@ -0,0 +1,6 @@
+#Cada novo termo na sequência de Fibonacci é gerado pela soma dos dois termos anteriores.
+#Começando com 1 e 2, os primeiros 10 termos serão:
+# 1, 2, 3, 5, 8, 13, 21, 34, 55, 89
+#Considerando os termos na sequência de Fibonacci cujos valores não excedem quatro milhões,
+#encontre a soma dos termos de valor par.
+
