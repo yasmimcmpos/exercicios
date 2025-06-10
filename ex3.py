6d3d984b86da3c10b01a4b213df602711c99b17f
yasmimcmpos <yasmimfer987@outlook.com>
2025-06-10 09:00:30 -0300

    correção de limites

--- ex3.py
+++ ex3.py
@@ -4,17 +4,22 @@
 #Considerando os termos na sequência de Fibonacci cujos valores não excedem quatro milhões,
 #encontre a soma dos termos de valor par.
 
+limite = 4000000
+
 def pares_e_soma(limite):
     a = 1
     b = 2
     soma = 0
 
     while a <= limite:
-        if a % 2 == 0:
-            soma += a
-        a, b = b, a + b
+        if a % 2 == 0: ##Módulo para descobrir se é divisisivel por 2
+            soma += a  #
+        a_novo = b
+        b_novo = a+b
+
+        a = a_novo
+        b = b_novo
     return soma
 
-limite = 4000000
 resultado = pares_e_soma(limite)
-print(f"Somar os termos pares até {limite}: {resultado}")
+print(f"Somar os termos pares até chegar em {limite} è igual a: {resultado}")
