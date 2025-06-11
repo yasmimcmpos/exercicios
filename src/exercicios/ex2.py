db7ac99c1e7589ca590f383c17bcdbd24692eb49
yasmimcmpos <yasmimfer987@outlook.com>
2025-06-11 18:07:54 -0300

    implementada a documentação de texto, para explicar as funções

--- src/exercicios/ex2.py
+++ src/exercicios/ex2.py
@@ -1,4 +1,4 @@
-'''
+"""
 Dado os nomes dos alunos junto com a série em que estão, crie uma lista para a escola.
 
 *Adicione o nome de um aluno à lista de uma nota:
@@ -30,53 +30,84 @@ sua implementação deverá indicar que isso está incorreto.
 por meio de uma Escola em Python.
 Se você não estiver familiarizado com classes em Python, as classes da
 documentação do Python são um bom lugar para começar.class
-'''
+"""
+
+# Quando Usar Classes?
+# Quando você precisa modelar objetos do mundo real (ex: Carro, Produto, Usuário).
+# Para evitar repetição de código (herança).
+# Quando quer organizar funções e dados relacionados em uma única estrutura.
 
 
-#Quando Usar Classes?
-#Quando você precisa modelar objetos do mundo real (ex: Carro, Produto, Usuário).
-#Para evitar repetição de código (herança).
-#Quando quer organizar funções e dados relacionados em uma única estrutura.
-
+import bisect
 
 from typing_extensions import Dict
-import bisect
-class Escola:
-    def __init__(self): #metodo contrutor
-        self.__aluno_serie__:Dict[int, list[str]] ={}
 
-    def adicionar_aluno(self, nome:str, serie:int):
+
+class Escola:
+    def __init__(self) -> None:  # metodo contrutor
+        self.__aluno_serie__: Dict[int, list[str]] = {}
+
+    def adicionar_aluno(self, nome: str, serie: int) -> None:
+        """Essa função vai adicionar um aluno a uma serie ou apresentar erro caso o aluno já esteja matriculado
+        em alguma serie
+
+                Args:
+                    nome (str): _description_
+                    serie (int): _description_
+
+                Raises:
+                    Exception: Caso o aluno já esteja matriculadoem alguma série
+        """
         if serie not in self.__aluno_serie__:
             self.__aluno_serie__[serie] = []
 
         if nome in self.__aluno_serie__[serie]:
             raise Exception(f"Erro: {nome} já está matriculado na {serie} serie")
 
-        bisect.insort(self.__aluno_serie__[serie], nome) ##TODO
+        bisect.insort(self.__aluno_serie__[serie], nome)
 
-    def alunos_na_serie(self, serie:int):
+    def alunos_na_serie(self, serie: int) -> list[str]:
+        """Essa função busca todos os alunos de uma série selecionada
+
+        Args:
+            serie (int): _description_
+
+        Raises:
+            Exception: Caso não tenha nenhum aluno na serie
+
+        Returns:
+            list[str]: todos os alunos da serie
+        """
         if serie in self.__aluno_serie__:
             return self.__aluno_serie__[serie]
         else:
-           raise Exception(f"Não tem alunos")
+            raise Exception(f"Não tem alunos")
 
-    def mostra_todos_alunos(self):
-        resultado: list[str]=[]
-        for serie in sorted(self.__aluno_serie__.keys()): #sorted serve para colocar na ordem
-            resultado.extend(self.__aluno_serie__[serie]) #add final da lista
+    def mostra_todos_alunos(self) -> list[str]:
+        """Essa função lista em ordem alfabética todos os alunos nas respectivas serie
+
+        Returns:
+            list[str]: a lista com todos os nomes
+        """
+        resultado: list[str] = []
+        for serie in sorted(
+            self.__aluno_serie__.keys()
+        ):  # sorted serve para colocar na ordem
+            resultado.extend(self.__aluno_serie__[serie])  # add final da lista
         return resultado
 
-escola = Escola() # instancia - inicializa um objeto
 
-escola.adicionar_aluno('Anna', 1)
-escola.adicionar_aluno('Barb', 1)
-escola.adicionar_aluno('Charlie', 1)
-escola.adicionar_aluno('Alex', 2)
-escola.adicionar_aluno('Peter', 2)
-escola.adicionar_aluno('Zoe', 2)
-escola.adicionar_aluno('Jim', 5)
+escola = Escola()  # instancia - inicializa um objeto
+
+escola.adicionar_aluno("Anna", 1)
+escola.adicionar_aluno("Barb", 1)
+escola.adicionar_aluno("Charlie", 1)
+escola.adicionar_aluno("Alex", 2)
+escola.adicionar_aluno("Peter", 2)
+escola.adicionar_aluno("Zoe", 2)
+escola.adicionar_aluno("Jim", 5)
 
 print(escola.alunos_na_serie(2))
 print(escola.mostra_todos_alunos())
-print(escola.adicionar_aluno('Jim', 2)) #'none' de resposta?
-print(escola.adicionar_aluno('Jim', 5))
+print(escola.adicionar_aluno("Jim", 2))  #'none' de resposta?
+print(escola.adicionar_aluno("Jim", 5))
