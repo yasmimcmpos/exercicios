07295f37c08f69d0d6ec96eba57f2a55cd6fee03
yasmimcmpos <yasmimfer987@outlook.com>
2025-06-12 16:12:06 -0300

    iniciando exercicio 4

---
+++ src/ex4.py
@@ -0,0 +1,12 @@
+# Encontre a diferença entre o quadrado da soma e a soma dos quadrados dos
+# primeiros N números naturais
+
+# O quadrado da soma dos dez primeiros números naturais é:
+# (1 + 2 + ... + 10)² = 55² = 3025
+
+# A soma dos quadrados dos dez primeiros números naturais é:
+# 1² + 2² + ... + 10² = 385
+
+# Portanto, a diferença entre o quadrado da soma dos dez primeiros
+# números naturais e a soma dos quadrados dos dez primeiros números naturais é
+# 3025 - 385 = 2640
