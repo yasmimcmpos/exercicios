ff619f73030b3cea513f92e0908b8a7c1aa64a80
yasmimcmpos <yasmimfer987@outlook.com>
2025-07-02 11:01:03 -0300

    iniciando exercicio 6

---
+++ src/exercicios/ex6.py
@@ -0,0 +1,5 @@
+# Um número palindrômico é lido da mesma forma nos dois sentidos.
+# O maior palíndromo feito do produto de 2 números de dígitos é:
+# 9009 = 91 x 99
+# Encontre o maior palíndromo feito do produto de 3
+# números de dígitos.
