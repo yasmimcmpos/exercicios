ea08d242d21b0368847fa725b1c1f63903df7304
yasmimcmpos <yasmimfer987@outlook.com>
2025-07-18 14:28:57 -0300

    inicio dos testes

--- tests/test_ex8.py
+++ tests/test_ex8.py
@@ -1,2 +1,14 @@
+from exercicios.ex8 import(
+    years_mercury,
+    years_venus,
+    years_mars,
+    years_jupter,
+    years_saturn,
+    years_uranus,
+    years_neptune,
+)
+
+def test_years_mercury():
+    assert years_mercury(1_000_000_000) == 
 # assert entrada_em_segundos_terra(1_000_000_000) == 31,68
 # assert entrada_em_segundos_marte(1_000_000_000) == 16,85
