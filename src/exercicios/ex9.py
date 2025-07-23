217556d1cee73e97080a9922466e36f5562e5505
yasmimcmpos <yasmimfer987@outlook.com>
2025-07-23 11:45:53 -0300

    Começando exercicios e adicionando documentação

--- src/exercicios/ex9.py
+++ src/exercicios/ex9.py
@@ -1,12 +1,45 @@
+# MMC
 # 2520  is the smallest number that can be divided by each of the numbers from
 # 1 to 10 without any remainder.
 # What is the smallest positive number that is evenly divisible by all of
 # the numbers from 1 to 20?
 
-def smallest_num_ex(num: int):
+import math
+from functools import reduce
 
 
-# Rodar uma lista de número de 1 até 10_000,
-# Dividi-los por todos os numeros de 1 a 20,
-# Identificar quais são divididos dentro do parâmetro sem restar,
-# Identificar o menor numero dos divididos sem resto
+def calc_mmc(a: int, b: int) -> int:
+    """calculo de mmc
+
+    Args:
+        a (int): primeiro número a ser multiplicado
+        b (int): segundo número a ser multiplicado
+
+    Returns:
+        int: resultado do MMC inteiro
+    """
+    return abs(a * b) // math.gcd(a, b)
+
+
+def mmc_in_list(lista: list[int]) -> int:
+    """Calcular o MMC dentro da lista
+
+    Args:
+        lista (list[int]): Lista de 1 a 10
+
+    Returns:
+        int: acumulo dos MMcs de todos os num da lista
+
+    Observations:
+        -A função 'reduce' pega os dois primeiro num e vai multiplicando-os
+        cumulativamente com os seguintes da sequência.
+
+    """
+    return reduce(calc_mmc, lista)
+
+
+# Função 1:
+# Calcular o MMC de dois em dois, calcula dois e passa para o próximo
+
+# Função 2:
+# Criar uma lista e aplicar o MMC nela para todos os números
--- tests/test_ex9.py
+++ tests/test_ex9.py
@@ -1,8 +1,14 @@
-from exercicios.ex9 import smallest_num, smallest_num_ex
+from exercicios.ex9 import calc_mmc, mmc_in_list
 
 
-def test_smallest_number_divided_ex():
+def test_ex():
     assert smallest_num_ex == 2520
 
 
+def test():
+    assert smallest_num == 232_792_560
+
+
 # testar se o menor num dividido sem resto realmente foi o de exemplo
+# range para percorrer minha lista de 1 a 11 e 1 a 21
+# Range: range(inicio, fim, passos)
