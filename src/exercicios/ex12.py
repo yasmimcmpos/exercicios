6b211d21e694bfa6e824328532917cf01faa822f
yasmimcmpos <yasmimfer987@outlook.com>
2025-08-08 15:52:31 -0300

    Feita logica do ex12 para criptografar uma letra por vez

--- src/exercicios/ex12.py
+++ src/exercicios/ex12.py
@@ -2,13 +2,24 @@
 # the Caesar cipher.
 # .index - serve para encontrar a posição de uma string ou lista
 # wrap around - útil para alfabeto, listas circulares ou relógios,
-#
 
 
-alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-char = "H"
-key = 3
-atual_position = alphabet.index(char)
-new_position = atual_position + key
-new_char = alphabet[new_position]
-print(new_position, new_char)
+def cesar(letter: str, key: int) -> str:
+    """Applies Caesar cipher to a single letter.
+
+    Args:
+        letter (str): A single alphabetic character.
+        key (int): The shift value for the cipher.
+
+    Returns:
+        str: The encrypted character.
+    """
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    letter = letter.upper()
+
+    if letter in alphabet:
+        atual_position = alphabet.index(letter)
+        new_position = (atual_position + key) % len(alphabet)  # 26
+        return alphabet[new_position]
+    else:
+        return letter
--- tests/test_ex12.py
+++ tests/test_ex12.py
@@ -1,5 +1,11 @@
-from exercicios.ex12 import algo
+from exercicios.ex12 import cesar
 
 
-def test_rotation():
-    assert Rot13() omg==trl
+def test_cesar() -> None:
+    key = 3
+    assert cesar("Y", key) == "B"
+
+
+# def test_cesar_frase() -> None:
+#     key = 3
+#     assert cesar("Hello World!", key) == "Khoor Zruog!"
