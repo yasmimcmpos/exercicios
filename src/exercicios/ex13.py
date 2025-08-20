540ff84d7ef79fd419f51ea2e5565e02712eb4da
yasmimcmpos <yasmimfer987@outlook.com>
2025-08-19 21:14:17 -0300

    Feita alterações na função de multiplicação do ex13, criado docstrings iniciando codigos de test

--- src/exercicios/ex13.py
+++ src/exercicios/ex13.py
@@ -12,28 +12,69 @@ class ComplexNumber:
         self.real = real_part
         self.imag = imag_part
 
-    def addition(self, other: ComplexNumber) -> ComplexNumber:
-        new_real = self.real + other.real
-        new_imag = self.imag + other.imag
-        return ComplexNumber(new_real, new_imag)
+    def __add__(self, other: ComplexNumber) -> ComplexNumber:
+        """Addition of ComplexNumbers
 
-    def subtraction(self, other: ComplexNumber) -> ComplexNumber:
-        new_real = self.real - other.real
-        new_imag = self.imag - other.imag
-        return ComplexNumber(new_real, new_imag)
+        Args:
+            other (ComplexNumber): other set
 
-    def multiplication(self, other: "ComplexNumber") -> ComplexNumber:
-        # (a + bi)(c + di)
+        Returns:
+            ComplexNumber: new added parts
+        """
+        return ComplexNumber(self.real + other.real, self.imag + other.imag)
+
+    def __sub__(self, other: ComplexNumber) -> ComplexNumber:
+        """Subtraction of Complex Numbers
+
+        Args:
+            other (ComplexNumber): other set
+
+        Returns:
+            ComplexNumber: new subtracted parts
+        """
+        return ComplexNumber(self.real - other.real,self.imag - other.imag)
+
+    def __mul__(self, other: "ComplexNumber") -> ComplexNumber:
+        """Distributive Multiplication = (a + bi)(c + di)
+
+        Args:
+            other (ComplexNumber): other set
+
+        Returns:
+            ComplexNumber: new multiplied parts
+        """
         ac = self.real * other.real
         ad = self.real * other.imag
         bc = self.imag * other.real
         bd = self.imag * other.imag
 
-        new_real = ac * bd
-        new_imag = ad * bc
-        return ComplexNumber(new_real, new_imag)
+        return ComplexNumber(ac - bd, ad + bc)
 
-    def absolute_value():
-    def conjugate(self):
-    def division(self, other):
+    def __truediv__(self, other:ComplexNumber) -> ComplexNumber:
+        """Division of complex numbers
+
+        Args:
+            other (ComplexNumber): other set
+
+        Returns:
+            ComplexNumber: new divided parts
+        """
+        denominator = other.real**2 + other.imag**2
+        real_part = (self.real * other.real + self.imag * other.imag) / denominator
+        imag_part = (self.real * self.imag + self.imag * self.real) / denominator
+        return ComplexNumber(real_part, imag_part)
+
+    def conjugate(self)-> ComplexNumber:
+        """conjugate complex number
+
+        Returns:
+            ComplexNumber: mantém a parte real e inverte o sinal da parte imaginária.
+        """
+        return ComplexNumber(self.real, -self.imag)
+
+    def absolute_value(self, power):
+
+
+
+
     def exponentiation():
--- tests/test_ex13.py
+++ tests/test_ex13.py
@@ -1,1 +1,5 @@
 from exercicios.ex13 import ComplexNumber
+
+
+def test_add() -> None:
+    assert ComplexNumber(__add__(2 + 2i))
