0cbfc5a444e2e35f3cbdd772ef3140aa2f4d7560
yasmimcmpos <yasmimfer987@outlook.com>
2025-09-15 10:15:22 -0300

    adcionados testes ex16

--- tests/test_ex16.py
+++ tests/test_ex16.py
@@ -16,3 +16,10 @@ def test_find_prime_at_position(position: int, expected: int) -> None:
     # Teste se find_prime_at_position retorna o número primo correto em
     # uma posição
     assert find_prime_at_position(position) == expected
+
+
+# OU
+
+
+def test_position() -> None:
+    assert find_prime_at_position(10_001) == 104_743
