1430161088d4c233d197c895a0d295ca5aebd44a
yasmimcmpos <yasmimfer987@outlook.com>
2026-02-09 12:57:46 -0300

    Ex23 - resolução

--- src/exercicios/ex23.py
+++ src/exercicios/ex23.py
@@ -1,12 +1,42 @@
 # https://exercism.org/tracks/python/exercises/acronym
 # Convert a phrase to its a acronym
-#  In cases of "-" we treat it as a space
-
-# Lógica inicial:
-# 1 - Se tiver um "-" tranformar ele em espaço com o (replace)
-# 2 - Separar a frase em palavras (split)
-# 3 - Pegar a primeira letra de cada palavra separada usando indice e
-# transformar ela em Maiuscula com (.upper) Tranformando ao final uma lista de letras
-# 4 - Juntar a lista de palavras de cada frase com (join)
+import re
 
 
+def convert(phrase: str) -> str:
+    """Converte uma frase em um acrônimo.
+
+    Args:
+        phrase (str): Frase de entrada a ser convertida em acrônimo.
+
+    Returns:
+        str: Acrônimo formado pelas iniciais das palavras da frase.
+
+    Obs:
+        Regras aplicadas:
+        1-Substitui hífens ("-") por espaços, tratando palavras compostas
+       como palavras separadas.
+
+        2-Remove caracteres especiais e pontuações, mantendo apenas letras
+       e espaços.
+
+        3-Separa a frase em palavras utilizando espaços como delimitador.
+
+        4-Extrai a primeira letra e converte todas as letras extraídas para maiúsculas.
+
+        5-Junta as letras em uma única string representando o acrônimo final.
+
+        r - não interpretar
+        A-Az-z - Letras maiúsculas e minúsculas
+        \s - espaços, tabs..
+
+    """
+    phrase = phrase.replace("-", " ")
+
+    phrase = re.sub(r"[^A-Za-z\s]", "", phrase)
+
+    words = phrase.split()
+
+    acronym_letters = [word[0].upper() for word in words]
+
+    return "".join(acronym_letters)
--- tests/test_ex23.py
+++ tests/test_ex23.py
@@ -1,7 +1,13 @@
-import exercicios.ex23 from convert....
+from exercicios.ex23 import convert
 
-def test_convert_phrases():
+
+def test_convert_phrases() -> None:
     assert convert("As Soon As Possible") == "ASAP"
-    assert convert("Liquid-crystal display") == "LCP"
+    assert convert("Liquid-crystal display") == "LCD"
     assert convert("Thank George it's Friday!") == "TGIF"
     assert convert("Portable Network   Graphics") == "PNG"
+    assert convert("rock & roll") == "RR"
+    assert convert("hello_world") == "H"
+    assert convert("  Hello World  ") == "HW"
+    assert convert("Complementary-metal-oxide semiconductor") == "CMOS"
+    assert convert("!!! --- ???") == ""
